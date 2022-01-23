2c3dfc6844eb37665f12ceabc6db744b4ff3a934
RuslanChuprov <chrv1828@gmail.com>
2022-01-23 18:27:05 +0200

    hw1-1

--- script1.js
+++ script1.js
@@ -1,2 +1,17 @@
 const a = 'Переменная а в script1';
-console.log(a);
+const price = 15;
+
+// console.log(a);
+// console.log('цена =', price);
+// const type = typeof 'lhvljhvv';
+// console.log(type);
+// const time = confirm('Хотите продлить');
+// console.log(time);
+
+// const prices = prompt('введите цену');
+// console.log(Number(prices));
+
+let width = "50.456px";
+// const result = Number.parseFloat(width);
+const result = 65165.53436;
+console.log(result.toFixed(3));
